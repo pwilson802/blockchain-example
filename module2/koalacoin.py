d182bd475875d42594b20d7c35e818e46a105ce5
Paul Wilson <paul.wilson@suez.com>
2020-03-16 09:02:49 +1100

    added routes for nodes

--- module2/koalacoin.py
+++ module2/koalacoin.py
@@ -147,9 +147,35 @@ def add_transaction():
     if not all (key in json for key in transaction_keys):
         return 'Some elements of the transactions are missing', 400
     index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transactions will be added to Block {index}'}
+    return jsonify(response), 201
 
 # Part 3 - Decentralizing the Blockchain
 
+# Connecting new nodes
+@app.route('/connect_node', method = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes in None:
+        return 'No node', 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The Koalacoin blockchain now contains the following nodes' ,
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', method = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'The nodes had different chains so the chain was replaced by the longest one',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one.',
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
 
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
